99f785e6c29106ee64b37d72569c8a4614475a5f
Komla Adzam <adzamkomla.dev@gmail.com>
2020-10-31 00:21:11 +0000

    add initial changes

---
+++ Models/Room.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace RazorPagesRoomReservations.Models
+{
+    public class Room
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int NumberOfOccupants { get; set; }
+        public string Condition { get; set; }
+        public string Description { get; set; }
+        public bool IsAvailable { get; set; }
+        public decimal Price { get; set; }
+    }
+}
